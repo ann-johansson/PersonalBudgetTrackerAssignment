5dd9fdd36503288d232bc2c00fd6e9816cc07460
ann-johansson <ann.johansson.work@gmail.com>
2025-10-17 00:24:22 +0200

    Har gjort en Transaction klass med användbara attribut för transaktioner som skall ske i programmet. Den är inte klar ännu då en ShowInfo() metod skall implementeras som visar en klar transaktion, fixar dock denna efter BudgetManager klassen är klar så jag har all information jag behöver.

---
+++ Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudgetTrackerAssignment
+{
+    public class Transaction
+    {
+        string description;
+        decimal amount;
+        string date;
+        string category;
+
+
+        //Fixa public void ShowInfo() här
+        //Tips! Färga text, Console.ForegroundColor = ConsoleColor.Green;  Console.ResetColor();
+    }
+}
